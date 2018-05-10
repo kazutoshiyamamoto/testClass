9bf587fd37d39e277138cc00393082949a554b00
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-10 17:43:50 +0900

    プロトコルの採用

--- testClass/testClass/File.swift
+++ testClass/testClass/File.swift
@@ -14,5 +14,22 @@ protocol GameProtocol {
     func miss()
 }
 
-
+class MyGame:GameProtocol {
+    private var total = 0
+    // プロトコルに従って実装するプロパティ
+    var gamePoint: Int {
+        get {
+            return total
+        }
+    }
+    //プロトコルに従って実装するメソッド
+    func hit() {
+        total += 10
+        print("ヒットしました。　+10")
+    }
+    func miss() {
+        total /= 2
+        print("ミスった！半減")
+    }
+}
 
