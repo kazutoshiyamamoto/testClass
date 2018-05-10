616456bde349e66c0f751fa5d03e9ae43abe15ed
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-10 17:43:11 +0900

    プロトコルの定義

---
+++ testClass/testClass/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  testClass
+//
+//  Created by home on 2018/05/10.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+protocol GameProtocol {
+    var gamePoint:Int { get }
+    func hit()
+    func miss()
+}
+
+
+
--- testClass/testClass/ViewController.swift
+++ testClass/testClass/ViewController.swift
@@ -8,45 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let obj = Player()
-        obj.who = "佐藤"
-        obj.hello()
-        obj.bye()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-}
-
-class Player {
-    var name: String = ""
-    
-    func hello() {
-        print("やあ！" + name)
-    }
-}
-
-extension Player {
-    // nameをwhoでもアクセスできるようにする
-    var who: String {
-        get {
-            return name
-        }
-        set(value) {
-            name = value
-        }
-    }
-    // 新しいメソッドを追加する
-    func bye() {
-        print("またね！" + name)
     }
 }
 
