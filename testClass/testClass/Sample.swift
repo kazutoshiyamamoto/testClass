da5b7987c1d4351ca5183acee88c174c4a54cdbd
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-09-09 00:12:30 +0900

    クラスBookStoreの実装

--- testClass/testClass/Sample.swift
+++ testClass/testClass/Sample.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class Bool {
+class Book {
     let name: String
     init(name: String) {
         self.name = name
     }
 }
 
-
+internal class BookStore {
+    let book1 = Book(name: "Book1")
+    let book2 = Book(name: "Book2")
+    func getAllBooksName() -> (String, String) {
+        return (book1.name, book2.name)
+    }
+}
